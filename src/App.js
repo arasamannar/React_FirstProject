2d1814678f7731aae4187a9285ff1f41f2e38224
arasamannar <arasamannar@gmail.com>
2023-11-07 08:51:04 +0530

    added files realted to Arasamannar's project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import Message from './Component.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Message> </Message>
+    </div>
+  );
+}
+
+export default App;
